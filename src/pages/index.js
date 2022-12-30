5e1be10c474c8908aee15d9a91b00450a6e6dd1e
warmab13 <alonsosalcido13@gmail.com>
2022-12-30 11:46:25 -0600

    SHIP get server side props function on index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,3 +3,13 @@ export default function HomePage() {
     <div>HomePage</div>
   )
 }
+
+
+export async function getServerSideProps(){
+  const res = await fetch('http://localhost:3000/api/tasks');
+  const tasks = await res.json();
+  console.log("🚀 ~ file: index.js:9 ~ getServerSideProps ~ tasks", tasks)
+  return {
+    props: {}
+  }
+}
